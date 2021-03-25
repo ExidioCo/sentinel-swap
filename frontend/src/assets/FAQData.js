a400cd1edb17d02c6f83cf35daf2103fc9491a5e
Srinivas Baride <srini@exidio.co>
2021-03-25 20:19:04 +0530

    Updated FAQ

--- frontend/src/assets/FAQData.js
+++ frontend/src/assets/FAQData.js
@@ -1,7 +1,27 @@
 const data = [
     {
         question: 'How long will the swap window be open for?',
-        answer: 'The swap window will be open for 12 months. It will be closed on March 27, 2022.',
+        answer: 'The swap window will be open for over 12 months. It will be closed on March 27, 2022.',
+    },
+    {
+        question: 'When is Mainnet?',
+        answer: 'Mainnet is scheduled for March 27th 2021. Follow Sentinel announcements to stay up to date on the latest: https://t.me/Sentinel_Announcements.',
+    },
+    {
+        question: 'When will I receive staking rewards?',
+        answer: 'You will be able to stake/delegate tokens on mainnet. Staking rewards will be generated per block. You will be able to actively delegate tokens in the Sentinel Wallet.',
+    },
+    {
+        question: 'What are all of the ways I can swap my ERC-20 tokens to Cosmos based tokens?',
+        answer: 'You can swap through the MXC exchange or through the manual process.',
+    },
+    {
+        question: 'What happens if I successfully swapped my ERC-20 tokens and then I forget my Sentinel address password and Mnemonic?',
+        answer: 'Please ensure the tokens are transferred to your correct account/wallet address within the 12 month swap period. Swapping to the incorrect account/wallet address or forgetting your address password or Mnemonic may result in loss, which will not be the responsibility of SNT Foundation PTE. LTD. and/or it\'s affiliates or subsidiaries.',
+    },
+    {
+        question: 'How many Cosmos tokens will I receive for each ERC20 token I swap?',
+        answer: 'This is a 1:1 swap. For each ERC-20 token you will swap you will receive one (1) Sentinel Cosmos-based token.',
     },
 ];
 
